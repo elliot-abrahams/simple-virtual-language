7ed4d79fe45eb34a4b1ccc0dfca15d14fc212420
ElliotAbrahams <elliot.abrahams06@gmail.com>
2026-07-11 21:21:53 +0100

    implement 'run' and 'compile' commands for the CLI

--- src/Driver.h
+++ src/Driver.h
@@ -6,6 +6,7 @@
 
 constexpr std::string_view BYTECODE_FILE_EXTENSION = ".svmb";
 constexpr std::string_view ASSEMBLY_FILE_EXTENSION = ".svma";
+constexpr std::string_view PROGRAMMING_LANGUAGE_FILE_EXTENSION = ".sv";
 
 class Driver {
 
@@ -13,8 +14,12 @@ public:
     static int runCLI(int argc, char* argv[]);
 
 private:
+    static int run(const char* filePath);
+    static int compile(const char* filePath);
     static int assemble(const char* filePath);
     static int execute(const char* filePath);
+
+    static bool isValidFileExtension(const std::filesystem::path& filePath, const std::string_view* extension);
 
     static void handleUnsupportedFileExtensionError(const std::filesystem::path* path);
 };
