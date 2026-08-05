3326cf41da634626bc0d2e40a567a40caaa91cba
ElliotAbrahams <elliot.abrahams06@gmail.com>
2026-08-05 20:22:13 +0100

    set console to utf-8 string encoding

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,8 @@
 #include "compiler/Compiler.h"
 
 int main(int argc, char* argv[]) {
-    return Driver::runCLI(argc, argv);
-
-    const compiler::Compiler* compiler = new compiler::Compiler();
-
-    compiler->compile("../examples/test.sv");
-
-    return 0;
-
     // set output to UTF-8
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-
-    assembler::Assembler* assembler = new assembler::Assembler();
-    auto bytecode = assembler->assemble("../examples/vm-test.svma");
-    if (!bytecode.has_value()) {
-        return 500;
-    }
-    delete assembler;
-
-    VM* vm = new VM();
-    try {
-        vm->run(&bytecode.value());
-    } catch (VMError& e) {
-        vm->handleVMError(e);
-    }
-    vm->dumpState();
-    delete vm;
-    return 0;
+    return Driver::runCLI(argc, argv);
 }
